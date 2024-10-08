160b6c1b24ba44b1cf8de83f5bd4c00b74cad539
Nicolas <Nicolas@zedia.com.br>
2024-10-08 16:21:40 -0300

    Aula 2 parte 2

--- Aula02-Pacotes/go.mod
+++ Aula02-Pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo
 
 go 1.23.2
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- Aula02-Pacotes/main.go
+++ Aula02-Pacotes/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main(){
 	fmt.Println("Escrevi no main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("devbookgmail.com")
+	fmt.Println(erro)
 }
+
+//"go mod tidy" para limpar dependencia não utilizadas
