66515337156ce8658d8345ae4d2a46ff7c4fbfd7
Nicolas <Nicolas@zedia.com.br>
2024-10-09 15:40:28 -0300

    Lecture 6 about operators

---
+++ Aula06-Operadores/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Aritmeticos
+	// +, -, /, *, %
+
+	var number1 int16 = 10
+	var number2 int32 = 15
+	soma := number1+int16(number2)
+	fmt.Println(soma)
+
+	// Atribuição
+	// = , := 
+	
+	// Relacionais
+	// == , > , < , !=
+
+	// Lógicos
+	// && , || , !
+
+	// Unários
+	// ++ , += , -- , -= , *= , /=
+}
